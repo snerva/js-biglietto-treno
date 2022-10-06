24e4df2d85a59665642a6d987d36b01994779f6e
snerva <selenenerva@live.it>
2022-10-06 14:55:58 +0200

    train fare price calc

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+//chiedere all'utente il numero di km che intende percorrere
+const kmNumber = prompt('Quanti chilometri intendi percorrere?');
+console.log(kmNumber);
+//chiedere all'utente l'etá del passeggero
+const userAge = prompt('Quanti anni hai?');
+console.log(userAge);
+/*su queste ultime due informazioni*/
+//calcolare il prezzo del biglietto in base ai km (0.21/km), con sconto 20% minorenni e 40% over 65.
+const trainFare = kmNumber * 0.21;
+console.log(trainFare);
+
+let trainFarePrice;
+
+if (userAge <= 18){
+   trainFarePrice = trainFare * 20 / 100;
+   console.log(trainFarePrice);
+} else if (userAge >= 65){
+    trainFarePrice = trainFare * 40 / 100;
+    console.log(trainFarePrice);
+} else {
+    trainFarePrice = trainFare;
+    console.log(trainFarePrice);
+}
+
+//comunicarlo in forma umana
